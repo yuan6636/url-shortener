bedc2054b1cab9164b255dc381acaa9c90b7eb7d
yuan6636 <aleniverson777@gmail.com>
2023-10-10 11:41:09 +0800

    build: use express-handlebars(view engine)

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send(`Hello world!`)
+  res.render('index')
 })
 
 app.post('/shorten', (req, res) => {
